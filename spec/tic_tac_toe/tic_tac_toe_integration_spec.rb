850679a14163f44db269acd68a585bb1e2e79542
Oliver Martell <oliver.martell@gmail.com>
2012-10-27 18:39:01 +0100

    Check if a square has already been taken

--- spec/tic_tac_toe/tic_tac_toe_integration_spec.rb
+++ spec/tic_tac_toe/tic_tac_toe_integration_spec.rb
@@ -66,12 +66,26 @@ module TicTacToe
       ask_player :b
     end
 
+    it "doesn't allow players to do a move that belongs to another player" do
+      io.stub(:gets).and_return("0,1","0,1")
+      io.stub(:puts)
+      io.should_receive(:puts).with("That square has been already taken, please do another movement")
+
+      ask_player :a
+      ask_player :b
+    end
+
     Move = Struct.new(:x, :y)
 
     def ask_player(player)
       io.puts("Player #{player.to_s}:")
       user_input = io.gets || ""
       move = Move.new *user_input.split(",").map(&:to_i)
+
+      if moves.values.flatten.any?{ |m| m.x == move.x && m.y == move.y }
+        return io.puts("That square has been already taken, please do another movement")
+      end
+
       moves[player] << move
 
       xs = moves[player].map(&:x)
